920ba31fddb3a5b2cb25b210c47f47fcf3ee9f1b
Francis Santerre <francis.santerre@gmail.com>
2017-06-27 19:23:20 -0400

    Add initial unit tests files

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Mag_Products_Integration
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
