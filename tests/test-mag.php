6ff1570aabb7f21fa90de7d12a5786dad741a34f
Francis Santerre <francis.santerre@gmail.com>
2020-09-12 12:20:59 -0400

    Remove useless tests

--- tests/test-mag.php
+++ tests/test-mag.php
@@ -7,31 +7,6 @@
  */
 class MagTest extends WP_UnitTestCase {
 
-	function test_is_ready() {
-		$this->assertFalse( magepress()->is_ready() );
-
-		update_option( 'mag_products_integration_rest_api_url', true );
-		update_option( 'mag_products_integration_rest_api_url_validated', true );
-
-		$this->assertTrue( magepress()->is_ready() );
-	}
-
-	function test_is_module_installed() {
-		$this->assertFalse( magepress()->is_module_installed() );
-
-		update_option( 'mag_products_integration_rest_api_url_validated', true );
-		$this->assertFalse( magepress()->is_module_installed() );
-
-		update_option( 'mag_products_integration_default_store_code', true );
-		$this->assertFalse( magepress()->is_module_installed() );
-
-		update_option( 'mag_products_integration_magento_module_installed', true );
-		$this->assertFalse( magepress()->is_module_installed() );
-
-		update_option( 'mag_products_integration_stores_code', true );
-		$this->assertTrue( magepress()->is_module_installed() );
-	}
-
 	function test_output_colors_css() {
 		ob_start();
 		magepress()->output_colors_css();
